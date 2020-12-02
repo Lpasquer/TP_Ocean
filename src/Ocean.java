3e6daffc6bd0815f836c3649d1b661756023cc99
Lpasquer <71018559+Lpasquer@users.noreply.github.com>
2020-12-02 14:21:33 +0100

    Initial commit

---
+++ src/Ocean.java
@@ -0,0 +1,28 @@
+
+public class Ocean {
+
+	/* Constantes */
+
+	//TODO
+
+	/* Attributs */
+
+	//TODO
+
+	/* Constructeur du systeme d'emprunt */
+	Ocean() {
+
+		/* Instanciation des sites */
+		//TODO
+
+	}
+
+	/* Point d'entree du programme */
+
+	public static void main(String[] args) {
+
+		//TODO
+	}
+
+
+} // class Ocean
